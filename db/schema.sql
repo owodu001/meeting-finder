3e2f17017206694ba8c3ba519bc2fbe6f4539f93
Jesse Edwards <jesse.david.edwards.1987@gmail.com>
2020-01-07 14:13:01 -0600

    db

--- db/schema.sql
+++ db/schema.sql
@@ -3,11 +3,16 @@ CREATE DATABASE user_db;
 USE user_db;
 CREATE TABLE user_info (
     id INT NOT NULL AUTO_INCREMENT,
-    user_name NOT NULL VARCHAR (100),
+    first_name NOT NULL VARCHAR (50),
+    last_name  NOT NULL VARCHAR (50),
     days_sober INT (11),
-    time 
-    sponsor_name VARCHAR (100),
+    sponsor_first VARCHAR (50),
+    sponsor_last VARCHAR (50),
     sponsor_cell  VARCHAR (10),
+    meeting_id INT (10) NOT NULL,
+    type VARCHAR (100),
+    meeting_location VARCHAR (100),
+    directions VARCHAR (100),
     meeting_1 VARCHAR (20), 
     meeting_2 VARCHAR (20),
     meeting_3 VARCHAR (20),
@@ -17,5 +22,6 @@ CREATE TABLE user_info (
     meeting_7 VARCHAR (20),
     PRIMARY KEY (id)
 );
+
 
 
