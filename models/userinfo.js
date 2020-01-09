3cdca9aeeb1bddbcc85205fd1040466b0aacc064
Jesse Edwards <jesse.david.edwards.1987@gmail.com>
2020-01-08 23:03:26 -0600

    juserinfo

--- models/userinfo.js
+++ models/userinfo.js
@@ -1,93 +1,84 @@
-module.exports = function (sequelize, DataTypes) {
-    const userInfo = sequelize.define("user_info", {
-        first_name: {
-           type: DataTypes.STRING,
-            allowNull: false
-        },
+module.exports = function(sequelize, DataTypes) {
+  const UserInfo = sequelize.define("UserInfo", {
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-        last_name: {
-           type: DataTypes.STRING,
-           allowNull: false
-        },
-        
-        days_sober: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-         },
-         
-        sponsor_first: {
-            type: DataTypes.STRING,
-            allowNull: true
-         },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-        Sponsor_last: {
-            type: DataTypes.STRING,
-            allowNull: true
-         },
+    days_sober: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
 
-        sponsor_cell: {
-            type: DataTypes.STRING,
-            allowNull: true
-         },
+    sponsor_first: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
 
-        meeting_id: {
-           type: DataTypes.INTEGER,
-           allowNull: false
-        },
-        
-        types: {
-            type: DataTypes.STRING,
-            allowNull: false
-         },
-         
-        meeting_location: {
-            type: DataTypes.STRING,
-            allowNull: false
-         },
-            
-        
-        directions: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },    
+    Sponsor_last: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
 
-        meeting_1: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-   
-           },
-        meeting_2: {
+    sponsor_cell: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
 
-            type: DataTypes.INTEGER,
-            allowNull: true
-   
-           },
-        meeting_3: {
+    meeting_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
 
-            type: DataTypes.INTEGER,
-            allowNull: true   
-           },
-        meeting_4: {
+    types: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-            type: DataTypes.INTEGER,
-            allowNull: true  
-           },
-        meeting_5: {
+    meeting_location: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        
-        meeting_6: {
+    directions: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
 
-            type: DataTypes.INTEGER,
-            allowNull: true,
-        },
-        meeting_7: {
-            
-            type: DataTypes.INTEGER,
-            allowNull: true,
-        }
-    });
-return userInfo;
+    meeting_1: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    meeting_2: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    meeting_3: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    meeting_4: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    meeting_5: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+
+    meeting_6: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    meeting_7: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  });
+  return UserInfo;
 };
