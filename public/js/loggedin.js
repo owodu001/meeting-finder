86edc663c96dec4f64d3e07e09268be912e79992
Matt Beachey <mattbeachey@gmail.com>
2020-01-13 14:16:43 -0600

    fixed hamburger button for not logged in, fav meeting doesn't add by default

--- public/js/loggedin.js
+++ public/js/loggedin.js
@@ -38,7 +38,7 @@ function addToFavorites(id, i) {
 }
 
 //click handlers for about menu/block
-const aboutButtonEl = document.getElementById("about-box");
+const aboutButtonEl = document.getElementById("about-box2");
 const topBarEl = document.getElementById("bar1");
 const midBarEl = document.getElementById("bar2");
 const botBarEl = document.getElementById("bar3");
@@ -80,7 +80,20 @@ function showFavoriteMeetings() {
           axios.get(queryURL).then(function(response) {
             console.log(response);
             const meeting = response.data.meetings[0];
-            loginTextCont.innerHTML = `
+            if (meetingID === null) {
+              loginTextCont.innerHTML = `
+              <div class="row">
+              <div class="col">
+                  <div class="card favorite-meeting-container">
+                      <div class="card-header">
+                          <p class="meeting-title">Your saved meetings will appear here.</p>
+                      </div>
+                  </div>
+                </div>
+              </div>
+              `;
+            } else {
+              loginTextCont.innerHTML = `
                             <div class="row">
                             <div class="col">
                                 <div class="card favorite-meeting-container">
@@ -103,6 +116,7 @@ function showFavoriteMeetings() {
                             </div>
                         </div>
                             `;
+            }
           });
         }, 400);
       } else {
